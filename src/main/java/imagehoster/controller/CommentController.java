78bee0c7dfc6249a61847678365fab6ee7968038
joydeepmscs <joydeep.mscs@gmail.com>
2020-12-21 00:08:18 +0000

    changed comment created date type from Date to LocalDateTime

--- src/main/java/imagehoster/controller/CommentController.java
+++ src/main/java/imagehoster/controller/CommentController.java
@@ -7,14 +7,13 @@ import imagehoster.service.CommentService;
 import imagehoster.service.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @Controller
 public class CommentController {
@@ -26,13 +25,14 @@ public class CommentController {
 
 
     @RequestMapping(value="/image/{imageId}/{title}/comments", method = RequestMethod.POST)
-    public String newComment(@PathVariable("imageId") Integer imageId, @PathVariable("title") String title, Model model, @RequestParam("comment") String commentText, HttpSession session, Comment newComment) {
+    public String newComment(@PathVariable("imageId") Integer imageId, @PathVariable("title") String title, @RequestParam("comment") String commentText, HttpSession session) {
+        Comment newComment= new Comment();
         Image image = imageService.getImage(imageId);
         User user= (User) session.getAttribute("loggeduser");
         newComment.setText(commentText);
         newComment.setUser(user);
         newComment.setImage(image);
-        newComment.setCreatedDate(new Date());
+        newComment.setCreatedDate(LocalDateTime.now());
         commentService.createComment(newComment);
         return "redirect:/images/"+imageId+"/"+title;
     }
