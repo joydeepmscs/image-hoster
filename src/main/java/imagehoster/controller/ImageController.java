b60bc838a7659a217d4adbf555405811dafac798
joydeepmscs <joydeep.mscs@gmail.com>
2020-12-29 14:31:10 +0000

    fixed error for edit-image with no tags

--- src/main/java/imagehoster/controller/ImageController.java
+++ src/main/java/imagehoster/controller/ImageController.java
@@ -203,8 +203,10 @@ public class ImageController {
             tagString.append(tags.get(i).getName()).append(",");
         }
 
-        Tag lastTag = tags.get(tags.size() - 1);
-        tagString.append(lastTag.getName());
+        if (!tags.isEmpty()) {
+            Tag lastTag = tags.get(tags.size() - 1);
+            tagString.append(lastTag.getName());
+        }
 
         return tagString.toString();
     }
